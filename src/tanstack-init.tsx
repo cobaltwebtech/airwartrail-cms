999c6c6f14521bd7b9a16b2babd99d9071701955
Christopher Garza <43961806+cobaltwebtech@users.noreply.github.com>
2025-12-29 14:10:42 -0600

    Fixed the Import Path for queryClient

--- src/tanstack-init.tsx
+++ src/tanstack-init.tsx
@@ -1,7 +1,7 @@
 import { QueryClientProvider } from '@tanstack/react-query';
 import { createRouter, RouterProvider } from '@tanstack/react-router';
 import ReactDOM from 'react-dom/client';
-import { queryClient } from '@/lib/trpc';
+import { queryClient } from '@/lib/query-client';
 import { routeTree } from '@/routeTree.gen';
 import './styles/global.css';
 
